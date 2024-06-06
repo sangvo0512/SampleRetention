d480dc7cfb3d80707546c519b4fe31bbb941f00f
VI0-Sang Vo <SangVo@pihlgp.com>
2024-06-06 09:24:26 +0700

    update code css home

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,32 +21,43 @@ const Home = () => {
     };
 
     return (
-        <div className="home-container">
-            <ButtonGroup className="home-button-group">
-                {user && user.role === 'admin' && (
-                    <>
-                        <Button variant="primary" onClick={() => navigateTo('/Member')}>
-                            <FaUser className="icon" /> {t('Home.Member')}
-                        </Button>
-                        <Button variant="primary" onClick={() => navigateTo('/Dept')}>
-                            <FaBuilding className="icon" /> {t('Home.Department')}
-                        </Button>
-                    </>
-                )}
-                <Button variant="primary" onClick={() => navigateTo('/Material')}>
-                    <FaCubes className="icon" /> {t('Home.Material')}
-                </Button>
-                <Button variant="primary" onClick={() => navigateTo('/Inventory')}>
-                    <FaBoxes className="icon" /> {t('Home.Inventory')}
-                </Button>
-                <Button variant="primary" onClick={() => navigateTo('/Catalog')}>
-                    <FaBook className="icon" /> {t('Home.Catalog')}
-                </Button>
-                <Button variant="danger" onClick={handleLogout}>
-                    <FaSignOutAlt className="icon" /> {t('Home.Logout')}
-                </Button>
-            </ButtonGroup>
+        <div>
+            <h2 className="home-title">{t('Home.title')}</h2>
+            <div className="home-container">
+                <ButtonGroup className="home-button-group-up">
+                    {user && user.role === 'admin' && (
+                        <>
+                            <Button className='home-btn' variant="primary" onClick={() => navigateTo('/Member')}>
+                                <FaUser className="icon" /> {t('Home.Member')}
+                            </Button>
+                        </>
+                    )}
+                    <Button className='home-btn' variant="primary" onClick={() => navigateTo('/Material')}>
+                        <FaCubes className="icon" /> {t('Home.Material')}
+                    </Button>
+                    <Button className='home-btn' variant="primary" onClick={() => navigateTo('/Inventory')}>
+                        <FaBoxes className="icon" /> {t('Home.Inventory')}
+                    </Button>
+
+                </ButtonGroup>
+                <ButtonGroup className="home-button-group-down">
+                    {user && user.role === 'admin' && (
+                        <>
+                            <Button className='home-btn' variant="primary" onClick={() => navigateTo('/Dept')}>
+                                <FaBuilding className="icon" /> {t('Home.Department')}
+                            </Button>
+                        </>
+                    )}
+                    <Button className='home-btn' variant="primary" onClick={() => navigateTo('/Catalog')}>
+                        <FaBook className="icon" /> {t('Home.Catalog')}
+                    </Button>
+                    <Button className='home-btn' variant="danger" onClick={handleLogout}>
+                        <FaSignOutAlt className="icon" /> {t('Home.Logout')}
+                    </Button>
+                </ButtonGroup>
+            </div>
         </div>
+
     );
 };
 
